77169340e99185e747ceef71f517bfad0f796fef
Jakob Drusany <drusany.jakob@gmail.com>
2024-12-04 19:49:29 +0100

    implemented contract(), TODO: check if it works

--- contractions.jl
+++ contractions.jl
@@ -3,82 +3,146 @@ using LinearAlgebra
 const Triangle = Tuple{Int,Int,Int}
 const Edge = Tuple{Int,Int}
 
+function triangleEdges(triangle::Triangle)
+    return [(triangle[1], triangle[2]), (triangle[2], triangle[3]), (triangle[1],triangle[3])]
+end
+
 function sortEdge(edge::Edge)
     return edge[1] < edge[2] ? edge : (edge[2], edge[1])
 end
 
+function pushToSetDict!(dict::Dict{K, Set{V}}, key::K, value::V) where {K, V}
+    if haskey(dict, key)
+        push!(dict[key], value)
+    else
+        dict[key] = Set([value])
+    end
+end
+
 function sortTriangle(triangle::Triangle)
-    return Tuple(sort(collect(triangle)))
+    if triangle[1] < triangle[2] < triangle[3]
+        return triangle
+    elseif triangle[1] < triangle[3] < triangle[2]
+        return (triangle[1], triangle[3], triangle[2])
+    elseif triangle[2] < triangle[1] < triangle[3]
+        return (triangle[2], triangle[1], triangle[3])
+    elseif triangle[2] < triangle[3] < triangle[1]
+        return (triangle[2], triangle[3], triangle[1])
+    elseif triangle[3] < triangle[1] < triangle[2]
+        return (triangle[3], triangle[1], triangle[2])
+    else
+        return (triangle[3], triangle[2], triangle[1])
+    end
 end
 
 struct SimplicialComplex2D
     coords::Array{Vector{Float64}}
     vertices::Set{Int}
-    edges::Set{Edge}
-    triangles::Set{Triangle}
 
-    _vertex_to_edges::Dict{Int,Array{Edge}}
-    _edge_to_triangles::Dict{Edge,Array{Triangle}}
+    _vertex_to_edges::Dict{Int,Set{Edge}}
+    _edge_to_triangles::Dict{Edge,Set{Triangle}}
 
     function SimplicialComplex2D(triangles::Array{Triangle}, coords::Array{Vector{Float64}})
-        vertices = Set{Int}()
-        edges = Set{Edge}()
-        triangles_sorted = Set{Triangle}()
-
-        _vertex_to_edges = Dict{Int,Array{Edge}}()
-        _edge_to_triangles = Dict{Edge,Array{Triangle}}()
+        _vertex_to_edges = Dict{Int,Set{Edge}}()
+        _edge_to_triangles = Dict{Edge,Set{Triangle}}()
 
         for triangle in triangles
             triangle = sortTriangle(triangle)
-            push!(triangles_sorted, triangle)
-            for edge in [(triangle[1], triangle[2]), (triangle[2], triangle[3]), (triangle[3], triangle[1])]
+            for edge in triangleEdges(triangle)
                 edge = sortEdge(edge)
-                push!(edges, edge)
-                if haskey(_edge_to_triangles, edge)
-                    push!(_edge_to_triangles[edge], triangle)
-                else
-                    _edge_to_triangles[edge] = [triangle]
-                end
+                pushToSetDict!(_edge_to_triangles, edge, triangle)
 
                 for vertex in edge
-                    push!(vertices, vertex)
-                    if haskey(_vertex_to_edges, vertex)
-                        push!(_vertex_to_edges[vertex], edge)
-                    else
-                        _vertex_to_edges[vertex] = [edge]
-                    end
+                    pushToSetDict!(_vertex_to_edges, vertex, edge)
                 end
             end
         end
 
-        new(coords, vertices, edges, triangles_sorted, _vertex_to_edges, _edge_to_triangles)
+        new(coords, keys(_vertex_to_edges), _vertex_to_edges, _edge_to_triangles)
     end
+end
+
+function edges(K::SimplicialComplex2D)
+    out = Set(union(values(K._vertex_to_edges)...))
+end
+
+function triangles(K::SimplicialComplex2D)
+    return Set(union(values(K._edge_to_triangles)...))
+end
+
+function removeEdge(K::SimplicialComplex2D, edge::Edge)
+    delete!(K._vertex_to_edges[edge[1]], edge)
+    delete!(K._vertex_to_edges[edge[2]], edge)
+end
+
+function removeTriangle(K::SimplicialComplex2D, triangle::Triangle)
+    for edge in [(triangle[1], triangle[2]), (triangle[2], triangle[3]), (triangle[3], triangle[1])]
+        delete!(K._edge_to_triangles[edge], triangle)
+    end
+end
+
+# replaces the old_vertex with the new_vertex in the simplicial complex K
+# does not handle adding new coords
+function replaceVertexInEdgesAndTriangles(K::SimplicialComplex2D, old_vertex::Int, new_vertex::Int)
+    delete!(K.vertices, old_vertex)
+    push!(K.vertices, new_vertex)
+
+    for edge in K._vertex_to_edges[old_vertex]
+        # construct the new edge with the new vertex and remove the old one
+        if edge[1] == old_vertex
+            new_edge = (new_vertex, edge[2])
+
+            # remove the edge from the other vertex that is not being replaced
+            delete!(K._vertex_to_edges[edge[2]], edge)
+        else
+            new_edge = (edge[1], new_vertex)
+            delete!(K._vertex_to_edges[edge[1]], edge)
+        end
+
+        new_edge = sortEdge(new_edge)
+
+        # add the new edge to the vertex_to_edges dictionary
+        pushToSetDict!(K._vertex_to_edges, new_edge[1], new_edge)
+        pushToSetDict!(K._vertex_to_edges, new_edge[2], new_edge)
+
+        for triangle in K._edge_to_triangles[edge]
+            new_triangle = sortTriangle(Tuple([vertex == old_vertex ? new_vertex : vertex for vertex in triangle]))
+            
+            # triangle is stored in 3 entries edges => triangles
+            for edge_of_trig in triangleEdges(triangle)
+                # we do not need to delete the triangle from the old edge
+                # because the old edge will be removed as a key
+                if edge_of_trig != edge
+                    delete!(K._edge_to_triangles[edge_of_trig], triangle)
+                    pushToSetDict!(K._edge_to_triangles, new_edge, new_triangle)
+                end
+            end
+        end
+        delete!(K._edge_to_triangles, edge)
+    end
+
+    delete!(K._vertex_to_edges, old_vertex)
 end
 
 struct ContractedSimplicialComplex2D
     original::SimplicialComplex2D
     contracted::SimplicialComplex2D
-    vertex_contracted_to_original::Dict{Int,Set{Int}}
 
     _contracted_triangle_Q::Dict{Triangle,Matrix{Float64}}
     _contracted_edge_Q::Dict{Edge,Matrix{Float64}}
     _contracted_vertex_Q::Dict{Int,Matrix{Float64}}
 
-    function ContractedSimplicialComplex2D(original, contracted, vertex_contracted_to_original)
+    function ContractedSimplicialComplex2D(original, contracted)
         _contracted_triangle_Q = Dict{Triangle,Matrix{Float64}}()
         _contracted_edge_Q = Dict{Edge,Matrix{Float64}}()
         _contracted_vertex_Q = Dict{Int,Matrix{Float64}}()
 
-        new(original, contracted, vertex_contracted_to_original, _contracted_triangle_Q, _contracted_edge_Q, _contracted_vertex_Q)
+        new(original, contracted, _contracted_triangle_Q, _contracted_edge_Q, _contracted_vertex_Q)
     end
 end
 
 function initialContractedSimplicialComplex2D(K::SimplicialComplex2D)
-    vertex_contracted_to_original = Dict{Int,Set{Int}}()
-    for vertex in K.vertices
-        vertex_contracted_to_original[vertex] = Set([vertex])
-    end
-    contracted = ContractedSimplicialComplex2D(K, deepcopy(K), vertex_contracted_to_original)
+    contracted = ContractedSimplicialComplex2D(K, deepcopy(K))
 
     calculateFundamentalQuadratics(contracted)
     return contracted
@@ -86,11 +150,11 @@ end
 
 # fills in the _triangle_Q, _edge_Q, and _vertex_Q fields of K
 function calculateFundamentalQuadratics(K::ContractedSimplicialComplex2D)
-    for triangle in K.contracted.triangles
+    for triangle in triangles(K.contracted)
         K._contracted_triangle_Q[triangle] = fundamental_quadratic([planeFromTriangle(triangle, K.contracted.coords)])
     end
 
-    for edge in K.contracted.edges
+    for edge in edges(K.contracted)
         neigh_triangles = K.contracted._edge_to_triangles[edge]
         K._contracted_edge_Q[edge] = sum([K._contracted_triangle_Q[triangle] for triangle in neigh_triangles])
     end
@@ -157,8 +221,32 @@ function getIncidentTriangles(K::SimplicialComplex2D, Vc::Set{Int})
     return incident_triangles
 end
 
-function errorOfEdgeInContractedComplex(K::ContractedSimplicialComplex2D, edge::Edge)
+function minCForEdgeContraction(K::ContractedSimplicialComplex2D, edge::Edge)
     Q = K._contracted_vertex_Q[edge[1]] + K._contracted_vertex_Q[edge[2]] - K._contracted_edge_Q[edge]
-    min_point = minEh(Q)
-    return Eh(min_point, Q)
+    return minEh(Q)
+end
+
+function contract(K::ContractedSimplicialComplex2D, edge::Edge)
+    # 1) remove ab, abx, and aby
+    # remove edge
+    delete!(K.contracted._vertex_to_edges[edge[1]], edge)
+    delete!(K.contracted._vertex_to_edges[edge[2]], edge)
+    
+    # remove triangles (each triangle is stored in 3 entries edges => triangles)
+    for triangle in K.contracted._edge_to_triangles[edge]
+        for edge_trig in triangleEdges(triangle)
+            delete!(K.contracted._edge_to_triangles[edge_trig], triangle)
+        end
+    end
+    delete!(K.contracted._edge_to_triangles, edge)
+
+    # 2) substitute c for a and for b wherever they occur in the remaining set of
+    # vertices, edges, and triangles (removing resulting duplications making sure L is a set.)
+    c_coords = minCForEdgeContraction(K, edge)
+    c = length(K.contracted.coords) + 1
+    push!(K.contracted.coords, c_coords)
+    
+    replaceVertexInEdgesAndTriangles(K.contracted, edge[1], c)
+    replaceVertexInEdgesAndTriangles(K.contracted, edge[2], c)
+    return K 
 end
--- contractions_tests.jl
+++ contractions_tests.jl
@@ -12,11 +12,71 @@ coords = [Vector([rand()*100, rand()*100,rand()*100]) for i in 1:9]
 torus = [(2,1,4), (4,1,3), (5,4,7), (6,5,8), (7,4,6), (7,1,8), (8,2,9), (7,9,1), (1,2,8), (4,5,2), (7,8,5), (3,6,4), (6,9,7), (1,9,3), (8,9,6), (3,2,5), (5,6,3), (2,3,9)]
 
 simplicial_complex = SimplicialComplex2D(torus, coords)
-println("Simplicial complex:")
-print_struct_properties(simplicial_complex)
+# println("Edges that contain 1")
+# arr = edges(simplicial_complex)
+# for edge in sort(collect(arr), by = x -> x[2])
+#     if edge[1] == 1 || edge[2] == 1
+#         println(edge)
+#     end
+# end
 
+# println("Edges that do not contain 1")
+# for edge in sort(collect(arr), by = x -> x[2])
+#     if edge[1] != 1 && edge[2] != 1
+#         println(edge)
+#     end
+# end
+
+# println("Triangles that contain 1")
+# arr = triangles(simplicial_complex)
+# for triangle in sort(collect(arr), by = x -> x[2])
+#     if triangle[1] == 1 || triangle[2] == 1 || triangle[3] == 1
+#         println(triangle)
+#     end
+# end
+
+# println("Triangles that do not contain 1")
+# for triangle in sort(collect(arr), by = x -> x[2])
+#     if triangle[1] != 1 && triangle[2] != 1 && triangle[3] != 1
+#         println(triangle)
+#     end
+# end
+
+# println("\nReplacing vertex 1 with vertex 10\n")
+# contracted = initialContractedSimplicialComplex2D(simplicial_complex)
+# replaceVertexInEdgesAndTriangles(contracted.contracted, 1, 10)
+
+# println("Edges that contain 10:")
+# arr = edges(contracted.contracted)
+# for edge in sort(collect(arr), by = x -> x[1])
+#     if edge[1] == 10 || edge[2] == 10
+#         println(edge)
+#     end
+# end
+
+# println("Edges that do not contain 10:")
+# for edge in sort(collect(arr), by = x -> x[2])
+#     if edge[1] != 10 && edge[2] != 10
+#         println(edge)
+#     end
+# end
+
+# println("Triangles that contain 10:")
+# arr = triangles(contracted.contracted)
+# for triangle in sort(collect(arr), by = x -> x[1])
+#     if triangle[1] == 10 || triangle[2] == 10 || triangle[3] == 10
+#         println(triangle)
+#     end
+# end
+
+# println("Triangles that do not contain 10:")
+# for triangle in sort(collect(arr), by = x -> x[2])
+#     if triangle[1] != 10 && triangle[2] != 10 && triangle[3] != 10
+#         println(triangle)
+#     end
+# end
+
+println(simplicial_complex)
 contracted = initialContractedSimplicialComplex2D(simplicial_complex)
-println("Initial contracted simplicial complex:")
-print_struct_properties(contracted)
-
-println(errorOfEdgeInContractedComplex(contracted, (1,2)))
+contracted = contract(contracted, (1, 2))
+println(contracted.contracted)
