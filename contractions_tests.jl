acf8c7971cabf90352533b65bf61d07475a5c695
Yon Ploj <admin@yon.si>
2024-12-11 23:21:32 +0100

    Add poper

--- contractions_tests.jl
+++ contractions_tests.jl
@@ -76,34 +76,40 @@ simplicial_complex = SimplicialComplex2D(torus, coords)
 #     end
 # end
 
-println(" === contract tests === ")
-println(simplicial_complex)
-contracted = initialContractedSimplicialComplex2D(simplicial_complex)
-c = contract!(contracted, (1, 2))
-println(" - the edge (1, 2) does not exist anymore")
-println("result: ", !haskey(contracted.contracted._edge_to_triangles, (1, 2)))
+# println(" === contract tests === ")
+# println(simplicial_complex)
+# contracted = initialContractedSimplicialComplex2D(simplicial_complex)
+# c = contract!(contracted, (1, 2))
+# println(" - the edge (1, 2) does not exist anymore")
+# println("result: ", !haskey(contracted.contracted._edge_to_triangles, (1, 2)))
 
-println(" - all edges have Q matrix")
-is_missing = false
-for edge in edges(contracted.contracted)
-    if !haskey(contracted._contracted_edge_Q, edge)
-        global is_missing
-        println("edge $edge does not have Q matrix")
-        is_missing = true
-    end
-end
-println("result: ", !is_missing)
+# println(" - all edges have Q matrix")
+# is_missing = false
+# for edge in edges(contracted.contracted)
+#     if !haskey(contracted._contracted_edge_Q, edge)
+#         global is_missing
+#         println("edge $edge does not have Q matrix")
+#         is_missing = true
+#     end
+# end
+# println("result: ", !is_missing)
 
-println(" - all triangles have Q matrix")
-is_missing = false
-for triangle in triangles(contracted.contracted)
-    if !haskey(contracted._contracted_triangle_Q, triangle)
-        global is_missing
-        println("triangle $triangle does not have Q matrix")
-        is_missing = true
-    end
-end
-println("result: ", !is_missing)
+# println(" - all triangles have Q matrix")
+# is_missing = false
+# for triangle in triangles(contracted.contracted)
+#     if !haskey(contracted._contracted_triangle_Q, triangle)
+#         global is_missing
+#         println("triangle $triangle does not have Q matrix")
+#         is_missing = true
+#     end
+# end
+# println("result: ", !is_missing)
+
+# println(" === contract tests === ")
+# println(simplicial_complex)
+# contracted = initialContractedSimplicialComplex2D(simplicial_complex)
+# contract!(contracted, (1, 2))
+# println(contracted.contracted)
 
 # println(" === getlink tests === ")
 # println(getlink(simplicial_complex, 1))
@@ -111,3 +117,40 @@ println("result: ", !is_missing)
 # println(getlink(simplicial_complex, (1, 2)))
 
 #println(error(contracted, (1, 2)))
+a = 5
+b = 6
+x = 2
+y = 9
+poper = [
+    (1, x, a),
+    (1, x, 3),
+    (x, a, b),
+    (x, b, 3),
+    (1, a, 4),
+    (a, 4, 8),
+    (a, 8, y),
+    (b, 3, 7),
+    (b, 7, 10),
+    (b, y, 8),
+    (b, y, 10),
+    (7, 10, y),
+    (4, 8, y)
+]
+poper_coordinates = [
+    [Float32(0.0), Float32(1.0), Float32(0.0)],  # "1"
+    [Float32(1.0), Float32(1.5), Float32(0.0)],  # "x"
+    [Float32(2.0), Float32(1.0), Float32(0.0)],  # "3"
+    [Float32(0.0), Float32(0.0), Float32(0.0)],  # "4"
+    [Float32(1.0), Float32(0.5), Float32(0.0)],  # "a"
+    [Float32(2.0), Float32(0.5), Float32(0.0)],  # "b"
+    [Float32(2.0), Float32(-0.5), Float32(0.0)], # "7"
+    [Float32(1.0), Float32(-1.0), Float32(0.0)], # "8"
+    [Float32(0.0), Float32(-0.5), Float32(0.0)], # "y"
+    [Float32(2.5), Float32(-1.5), Float32(0.0)]  # "0"
+]
+
+popercomplex = SimplicialComplex2D(poper, poper_coordinates)
+println(" === poper tests === ")
+contracted = initialContractedSimplicialComplex2D(popercomplex)
+contract!(contracted, (a, b))
+println(contracted.contracted)
